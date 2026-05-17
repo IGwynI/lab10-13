bd9adbbddceffc936b6680ae62217e641bb8fad7
IGwynI <roboandr16@gmail.com>
2026-05-17 20:09:42 +0500

    first commit

---
+++ Компилятор/Компилятор/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Компилятор")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Компилятор")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c5c7fb0b-bb12-4c29-ac44-3d4036b5c6ce")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Компилятор/Компилятор/TextPosition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компилятор
+{
+    struct TextPosition
+    {
+        private uint _lineNumber;
+        private byte _charNumber;
+
+        public uint LineNumber
+        {
+            get 
+            { 
+                return _lineNumber; 
+            }
+            set 
+            { 
+                _lineNumber = value; 
+            }
+        }
+
+        public byte CharNumber
+        {
+            get 
+            { 
+                return _charNumber; 
+            }
+            set 
+            { 
+                _charNumber = value; 
+            }
+        }
+
+        public TextPosition(uint ln = 0, byte c = 0)
+        {
+            _lineNumber = ln;
+            _charNumber = c;
+        }
+    }
+}
